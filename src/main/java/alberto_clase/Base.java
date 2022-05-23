e6a67df6db0c668572dcea9a6f3d9737deeee5ed
AlbertoGomez <albertogomezclase@gmail.com>
2022-05-23 10:30:16 +0200

    Añadido constructor vacío

--- src/main/java/alberto_clase/Base.java
+++ src/main/java/alberto_clase/Base.java
@@ -7,6 +7,9 @@ public class Base extends JLabel{
     
     int velocidadX=0;
     int velocidadY=0;
+    public Base(){
+        super();
+    }
 
     public Base(Icon imagen){
         super(imagen);
